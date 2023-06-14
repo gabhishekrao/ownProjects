fd314d13c9040c09d36c1f5c66d7e01bbb3d9e3f
gabhishekrao <119596349+gabhishekrao@users.noreply.github.com>
2023-06-14 16:46:05 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+//components
+import Header from './components/Header';
+import TodoForm from './components/TodoForm';
+
+function App() {
+  return (
+    <div >
+     <Header/>
+     <TodoForm/>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/database/db.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const USERNAME = process.env.DB_USERNAME
+const PASSWORD = process.env.DB_PASSWORD
+
+export const Connection = () => {
+
+    const MONGODB_URL = `mongodb+srv://${USERNAME}:${PASSWORD}@gabhishek01.dq23z80.mongodb.net/todoList`
+
+    mongoose.connect(MONGODB_URL, {useNewUrlParser: true});
+
+
+mongoose.connection.on('connected', () => {
+    console.log('Database is connected successfully.');
+})
+
+mongoose.connection.on('disconnected', () => {
+    console.log('Database disconnected.');
+})
+
+mongoose.connection.on('error', () => {
+    console.log('Error while connecting with database.', error.message);
+})
+
+}
+
+export default Connection;
